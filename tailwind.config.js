2493d7801fed7b3ffe67f2513519c350495172a7
nansmatty <narayanmaitysp1997@gmail.com>
2022-08-30 15:43:01 +0530

    halfway done

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ["./*.html", "./src/**/*.{html,js}"],
+	theme: {
+		extend: {},
+		screens: {
+			"2xl": { max: "1535px" },
+			// => @media (max-width: 1535px) { ... }
+			xl: { max: "1279px" },
+			// => @media (max-width: 1279px) { ... }
+			lg: { max: "1167px" },
+			// => @media (max-width: 1167px) { ... }
+			md: { max: "767px" },
+			// => @media (max-width: 767px) { ... }
+			sm: { max: "639px" },
+			// => @media (max-width: 639px) { ... }
+		},
+	},
+	plugins: [],
+};
